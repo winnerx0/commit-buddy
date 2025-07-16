5c078eb9ceb04b7e05471828555461a666e181dc
winnerx0 <eragbonikpeya@gmail.com>
2025-07-16 09:37:04 +0100

    feat: Generate commit messages using Gemini API
    - Added dependency `github.com/joho/godotenv` to load environment variables from a `.env` file.
    - Initialized `godotenv` in `init()` function to load the environment variables.
    - Created structs `AIResponse`, `ContentPart`, `ContentItem`, `GenerateReq`, and `Error` to handle request and response from the Gemini API.
    - Modified `main.go` to send a `git diff --staged` to the Gemini API and generate a commit message.
    - Added logic to handle the AI response and execute the commit command.

--- main.go
+++ main.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"os/exec"
+	"strings"
+
+	"github.com/joho/godotenv"
 )
 
-func main(){
+type AIResponse struct {
+	Candidates []struct {
+		Content struct {
+			Parts []struct {
+				Text string `json:"text"`
+			} `json:"parts"`
+		} `json:"content"`
+	} `json:"candidates"`
+}
 
+type ContentPart struct {
+	Text string `json:"text"`
+}
 
+type ContentItem struct {
+	Parts []ContentPart `json:"parts"`
+}
+
+type GenerateReq struct {
+	Contents []ContentItem `json:"contents"`
+}
+
+type Error struct {
+	Error struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+		Status  string `json:"status"`
+	} `json:"error"`
+}
+
+func init() {
+	godotenv.Load(".env")
+}
+
+func main() {
+
+	client := &http.Client{}
 
 	// _ := flag.String("g", "", "Generate a commit")
 
 	// flag.Parse()
-
 
 	command := exec.Command("git", "diff", "--staged")
 
@@ -28,5 +69,84 @@ func main(){
 		return
 	}
 
-	fmt.Println(string(diffOutput))
+	var reqBody = GenerateReq{
+		Contents: []ContentItem{
+			{
+				Parts: []ContentPart{
+					{
+						Text: fmt.Sprintf(
+							"Please use '%s' and create professional Git commit messages based on this diff.",
+							string(diffOutput),
+						),
+					},
+				},
+			},
+		},
+	}
+
+	bodyBytes, err := json.MarshalIndent(reqBody, "", "  ")
+	if err != nil {
+		fmt.Printf("failed to encode request body: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent", bytes.NewReader([]byte(bodyBytes)))
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	defer req.Body.Close()
+
+	req.Header.Add("X-goog-api-key", os.Getenv("GEMINI_API_KEY"))
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println("Error ", err)
+		return
+	}
+
+	if res.StatusCode != 200 {
+		var error Error
+
+		err = json.Unmarshal(body, &error)
+
+		if err != nil {
+			fmt.Println("Error parsing", err)
+			return
+		}
+		fmt.Println(error.Error.Message)
+		return
+	}
+
+	var aiResponse AIResponse
+
+	err = json.Unmarshal(body, &aiResponse)
+
+	if err != nil {
+		fmt.Println("Error parsing", err)
+		return
+	}
+
+	commit := strings.ReplaceAll(aiResponse.Candidates[0].Content.Parts[0].Text, "```", "")
+	
+	fmt.Println(commit)
+	fmt.Print("Press Enter to run this command, or Ctrl+C to cancel...")
+
+	fmt.Scanln()
+
+	cmd := exec.Command("git", "commit", "-m", commit)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+
 }
