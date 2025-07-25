c81b3ddc38759fcc7c749c1c8e2eb03c0a2f465d
winnerx0 <eragbonikpeya@gmail.com>
2025-07-25 09:52:32 +0100

    feat(commit-buddy): install commit-buddy from github - Modified `install.sh` to download and build commit-buddy from its GitHub repository. - Added `git clone` to fetch the commit-buddy source code. - Included steps to build the executable using `go build`. - Moved the executable to `/usr/local/bin` for global accessibility. - Removed the example `.env` file as it's not relevant to the new installation method. - Deleted the old `commit-buddy` binary.

--- install.sh
+++ install.sh
@@ -4,8 +4,22 @@ if [[ $EUID -ne 0 ]]; then
     echo "Please run this script as a root user. Use sudo"
     exit 1
 else
+    
+    git clone https://github.com/winnerx0/commit-buddy.git
+    
+    echo "Downloading Commit Buddy set up"
+    
+    cd commit-buddy
+    
+    current_dir=$(pwd)
+    
     go build -o cb .
+    
     mv ./cb /usr/local/bin
+    
+    cd ..
+    
+    # rm -r "$current_dir"
 fi
 
 echo "Installation complete please run cb to get started!"
